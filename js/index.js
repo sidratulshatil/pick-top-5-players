99f85af371f85cff9c2c12d465044111333c0b71
sidratulshatil <105915771+sidratulshatil@users.noreply.github.com>
2022-08-22 15:09:40 +0600

    Calculate Total btn event added

--- js/index.js
+++ js/index.js
@@ -2,16 +2,24 @@ const playerList = []
 
 
 function playerListUpdate(playersName) {
-
-
     const playersList = document.getElementById("players-list")
     playersList.innerText = "";
     for (let i = 0; i < playersName.length; i++) {
-        // console.log(playerList[i].name)
         const name = playerList[i].name
         const li = document.createElement("li")
         li.innerText = name
-        playersList.appendChild(li)
+
+
+        const playersList = document.getElementById("players-list")
+        // const playersListLength = playersList.children.length
+
+        // console.log(playersListLength)
+        if (playersList.children.length <= 4) {
+            playersList.appendChild(li)
+        }
+        else {
+            alert('You Cannot Add More Than 5 Players!!')
+        }
     }
 }
 
@@ -21,9 +29,7 @@ function addToPlayerList(input) {
 
     const playerListObj = {
         name: playerName
-
     }
-    // console.log(playerListObj.name)
     playerList.push(playerListObj)
     playerListUpdate(playerList)
     input.disabled = true
@@ -41,4 +47,14 @@ document.getElementById('btn-calculate').addEventListener('click', function () {
     const playerExpensesTotal = perPlayerField * playersListLength
 
     setTextOnTextField('player-expenses-field', playerExpensesTotal)
+})
+
+document.getElementById('btn-calculate-total').addEventListener('click', function () {
+    const managerCost = getElementById('manager-cost')
+    const coachCost = getElementById('coach-cost')
+    const playerExpensesField = getElementByTextId('player-expenses-field')
+    const totalCost = playerExpensesField + managerCost + coachCost
+
+    setTextOnTextField('total', totalCost)
+
 })
